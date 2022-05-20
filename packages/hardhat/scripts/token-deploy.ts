b0228f6c869977e650ae265cd03e0ba8c6e0d390
tonny0831 <tonnyjansen0831@gmail.com>
2022-05-20 17:28:08 +0300

    https://github.com/dethcrypto/TypeChain/tree/master/packages/hardhat
    https://hardhat.org/tutorial/

---
+++ packages/hardhat/scripts/token-deploy.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+  
+    console.log("Token address:", token.address);
+    console.log("Token owner:", await token.owner());
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
