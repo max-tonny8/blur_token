73d513813de5fc8ed774c738b9918913cd2a66f4
tonny0831 <tonnyjansen0831@gmail.com>
2022-05-21 15:16:01 +0300

    @rainbow-me/rainbowkit wagmi ethers
    https://www.rainbowkit.com/docs/installation
    https://wagmi.sh/docs/comparison

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,8 +1,44 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton,
+} from "@rainbow-me/rainbowkit";
+import { allChains, chain, createClient, WagmiConfig } from "wagmi";
+
+const { chains, provider } = configureChains(allChains, [
+  apiProvider.fallback(),
+]);
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          coolMode
+          showRecentTransactions={true}
+          chains={chains}
+        >
+          <ConnectButton />
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
